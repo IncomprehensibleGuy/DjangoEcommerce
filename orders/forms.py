277ef0c82cf58fcd1f4f2c2ec699a37ae813c71f
IncomprehensibleGuy <gregoryrulit@gmail.com>
2020-12-10 00:36:38 +0300

    orders app refactoring

--- orders/forms.py
+++ orders/forms.py
@@ -1,24 +1,33 @@
 from django import forms
 
-from .models import Order
+from .models import Order, ReceiveMethod
 
 
 class CreateOrderForm(forms.ModelForm):
+
+    RECEIVE_METHOD_CHOICES = [(method.title, method.title) for method in ReceiveMethod.objects.all()]
+
+    def get_default_receive_method(self, receive_method):
+        for choice in self.RECEIVE_METHOD_CHOICES:
+            if receive_method in choice:
+                return choice
+
+    payment_method = forms.ChoiceField(choices=Order.PAYMENT_METHOD_CHOICES, widget=forms.RadioSelect)
+    delivery_date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['city'].widget.attrs['class'] = 'form-control'
         self.fields['city'].widget.attrs['placeholder'] = 'Населённый пункт'
         self.fields['receive_method'].widget.attrs['class'] = 'select form-control'
+        #self.fields['receive_method'].choices = self.RECEIVE_METHOD_CHOICES
+        #self.fields['receive_method'].initial = self.get_default_receive_method('Самовывоз')
         self.fields['address'].label = 'Город, Улица, Номер дома, Корпус'
         self.fields['delivery_date'].label = 'Дата доставки'
         self.fields['payment_method'].widget.attrs['class'] = 'payment-list'
 
         self.payment_method_card = Order.PAYMENT_METHOD_CHOICES[0][1]
         self.payment_method_cash = Order.PAYMENT_METHOD_CHOICES[1][1]
-
-    payment_method = forms.ChoiceField(choices=Order.PAYMENT_METHOD_CHOICES, widget=forms.RadioSelect)
-    delivery_date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
 
     class Meta:
         model = Order
--- orders/views.py
+++ orders/views.py
@@ -4,6 +4,10 @@ from django.db import transaction
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 
+from shop.models import Category
+from cart.cart import Cart
+
+from .models import ReceiveMethod, OrderItem
 from .forms import CreateOrderForm
 
 
@@ -14,10 +18,8 @@ class CheckoutView(View):
         form = CreateOrderForm(request.POST or None)
 
         context = {
-            'cart': self.cart,
             'categories': categories,
             'form': form,
-            'total_order_price': self.cart.total_price,
             'receive_methods': ReceiveMethod.objects.all(),
         }
 
@@ -26,39 +28,40 @@ class CheckoutView(View):
 
 class CreateOrderView(View):
 
-    def fill_order_info(self, order, form, customer):
-        order.customer = customer
-        order.city = form.cleaned_data['city']
-        order.receive_method = form.cleaned_data['receive_method']
-        order.address = form.cleaned_data['address']
-        order.apartment_number = form.cleaned_data['apartment_number']
-        order.porch_number = form.cleaned_data['porch_number']
-        order.floor_number = form.cleaned_data['floor_number']
-        order.intercom = form.cleaned_data['intercom']
-        order.full_passport_name = form.cleaned_data['full_passport_name']
-        order.phone = form.cleaned_data['phone']
-        order.email = form.cleaned_data['email']
-        order.delivery_date = form.cleaned_data['delivery_date']
-        order.delivery_time = form.cleaned_data['delivery_time']
-        order.payment_method = form.cleaned_data['payment_method']
-        order.comment = form.cleaned_data['comment']
-        order.cart = self.cart
-        order.save()
-
     @transaction.atomic
     def post(self, request, *args, **kwargs):
         form = CreateOrderForm(request.POST or None)
-        customer = Customer.objects.get(user=request.user)
-
+        cart = Cart(request)
+        print(form.errors)
+        #customer = Customer.objects.get(user=request.user)
+        # print(form)
+        # print(dir(form))
+        #
         if form.is_valid():
-            self.cart.in_order = True
-            self.cart.save()
+            order = form.save(commit=False)
+            # order.customer = customer
+            order.city = form.cleaned_data['city']
+            order.receive_method = form.cleaned_data['receive_method']
+            order.address = form.cleaned_data['address']
+            order.apartment_number = form.cleaned_data['apartment_number']
+            order.porch_number = form.cleaned_data['porch_number']
+            order.floor_number = form.cleaned_data['floor_number']
+            order.intercom = form.cleaned_data['intercom']
+            order.full_passport_name = form.cleaned_data['full_passport_name']
+            order.phone = form.cleaned_data['phone']
+            order.email = form.cleaned_data['email']
+            order.delivery_date = form.cleaned_data['delivery_date']
+            order.delivery_time = form.cleaned_data['delivery_time']
+            order.payment_method = form.cleaned_data['payment_method']
+            order.comment = form.cleaned_data['comment']
+            #order.cart = Cart(request)
+            order.save()
 
-            new_order = form.save(commit=False)
-            self.fill_order_info(new_order, form, customer)
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+            # очистка корзины
+            cart.clear()
 
-            customer.phone = form.cleaned_data['phone']
-            customer.email = form.cleaned_data['email']
             # customer.addresses.add(
             #     Address(
             #         address=form.cleaned_data['address'],
@@ -68,10 +71,10 @@ class CreateOrderView(View):
             #         intercom=form.cleaned_data['intercom']
             #     )
             # )
-            customer.orders.add(new_order)
-            customer.save()
+            #customer.orders.add(new_order)
+            #customer.save()
 
             messages.add_message(request, messages.INFO, 'Спасибо за заказ! Менеджер с вами свяжется в течение 5 минут.')
             return HttpResponseRedirect('/')
-
-        return HttpResponseRedirect('/checkout/')
+        print('not valid form')
+        return HttpResponseRedirect('/orders/checkout/')
